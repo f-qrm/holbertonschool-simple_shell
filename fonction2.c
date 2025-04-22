97f372867e99c27bb19c88ee2f4d18a957ebc879
Alexiscnl <alexis.cornillon@gmail.com>
2025-04-22 14:28:35 +0200

    delete space line 78 in fonction2

--- fonction2.c
+++ fonction2.c
@@ -75,7 +75,7 @@ int execute_command(char **args)
 		cmd_path = search_path(args[0]);
 	if (cmd_path == NULL)
 	{
-		fprintf(stderr, "./hsh: 1: %s : not found\n", args[0]);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 		exit(127); }
 	pid = fork();
 	if (pid == -1)
